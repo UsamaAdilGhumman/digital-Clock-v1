bae1bd2b2e8c0aa2aff498b2bf41c0419bef6b37
UsamaAdilGhumman <usama.adil.ghumman@gmail.com>
2023-02-25 21:06:53 +0500

    Random Background Applied

--- index.js
+++ index.js
@@ -54,14 +54,14 @@ setInterval(InternationalTime, 1000);
 
 // Background Random
 
-// const body = document.body;
-// setInterval(() => {
-//   const red = Math.floor(Math.random() * 256);
-//   const blue = Math.floor(Math.random() * 256);
-//   const green = Math.floor(Math.random() * 256);
-//   const rgb = `rgb(${red},${green},${blue})`;
-//   body.style.backgroundColor = rgb;
-// }, 1000);
+const body = document.body;
+setInterval(() => {
+  const red = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const rgb = `rgb(${red},${green},${blue})`;
+  body.style.backgroundColor = rgb;
+}, 1000);
 
  // Stop Watch
 
